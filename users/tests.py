46e87b678aa1ba3fd0ee85d3a33c9d86cddc4a61
3961k <80889322+3961K@users.noreply.github.com>
2021-07-05 08:15:45 +0000

    add お気に入りした記事の一覧ページを作成
    対象のユーザのお気に入りに追加した記事一覧を表示するFavoriteArticleListViewを作成
    FavoriteArticleListViewに対するユニットテストの作成と検証
    プロフィールにおいて共通する部分をbase.htmlとして分割した

--- users/tests.py
+++ users/tests.py
@@ -3,7 +3,7 @@ from django.contrib.auth import get_user_model
 from django.urls import reverse
 from urllib.parse import urlencode
 
-from articles.models import Article, Tag
+from articles.models import Article
 
 # Create your tests here.
 
@@ -17,22 +17,12 @@ class PostedArticleListView(TestCase):
         User.objects.create_user(username='posted_view',
                                  email='posted_view@test.com',
                                  password='t0pp0gev1ew')
-        for i in range(1, 6):
-            Article.objects.create(author=User.objects.first(),
-                                   title='test_title{}'.format(i),
-                                   content='test_content{}'.format(i))
-
-        Tag.objects.create(tag='sample')
-        tag = Tag.objects.get(tag='sample')
-        User.objects.create_user(username='tag_posted_view',
-                                 email='tag_posted_view@test.com',
-                                 password='t0pp0gev1ew')
-        for i in range(1, 6):
-            Article.objects.create(author=User.objects.first(),
+        user = User.objects.get(username='posted_view')
+        for i in range(1, 7):
+            Article.objects.create(author=user,
                                    title='test_title{}'.format(i),
                                    content='test_content{}'.format(i))
             article = Article.objects.filter(title='test_title{}'.format(i))[0]
-            article.tags.add(tag)
             article.save()
 
         return super().setUpClass()
@@ -57,7 +47,7 @@ class PostedArticleListView(TestCase):
         self.assertEqual(response.status_code, 200)
 
         response2 = self.client.get(''.join([reverse('users:articles',
-                                             kwargs={'username': 'posted_view'}),
+                                                     kwargs={'username': 'posted_view'}),
                                              '?', urlencode(dict(page='2'))]))
         self.assertEqual(response2.status_code, 200)
 
@@ -67,3 +57,45 @@ class PostedArticleListView(TestCase):
                                            kwargs={'username': 'posted_view'}),
             '?', urlencode(dict(page='6'))]))
         self.assertEqual(response.status_code, 404)
+
+
+class FavoriteListViewTest(TestCase):
+    @classmethod
+    def setUpClass(cls):
+        User.objects.create_user(username='favorite_list_view',
+                                 email='favorite_list_view@test.com',
+                                 password='t0pp0gev1ew')
+        user = User.objects.get(username='favorite_list_view')
+        for i in range(1, 7):
+            Article.objects.create(author=User.objects.first(),
+                                   title='test_title{}'.format(i),
+                                   content='test_content{}'.format(i))
+            article = Article.objects.filter(title='test_title{}'.format(i))[0]
+            article.favorite_users.add(user)
+            article.save()
+
+        return super().setUpClass()
+
+    # favorite_list_viewユーザのプロフィールにアクセスする事が出来る
+    def test_success_access_page(self):
+        response = self.client.get(reverse('users:favorites',
+                                           kwargs={'username': 'favorite_list_view'}))
+        self.assertEqual(response.status_code, 200)
+
+    # 存在しないユーザ名のプロフィールにアクセスしようとすると404が返される
+    def test_fail_access_page(self):
+        response = self.client.get(reverse('users:favorites',
+                                           kwargs={'username': 'not_exist'}))
+        self.assertEqual(response.status_code, 404)
+
+    # /?page=1・/?page=2にアクセスする事が出来る
+    def test_success_access_page12(self):
+        response = self.client.get(''.join([reverse('users:favorites',
+                                           kwargs={'username': 'favorite_list_view'}),
+            '?', urlencode(dict(page='1'))]))
+        self.assertEqual(response.status_code, 200)
+
+        response2 = self.client.get(''.join([reverse('users:favorites',
+                                             kwargs={'username': 'favorite_list_view'}),
+                                             '?', urlencode(dict(page='2'))]))
+        self.assertEqual(response2.status_code, 200)
--- users/urls.py
+++ users/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import PostedArticleListView
+from .views import PostedArticleListView, FavoriteListView
 
 app_name = 'users'
 
 urlpatterns = [
     path('<str:username>', PostedArticleListView.as_view(), name='articles'),
+    path('<str:username>/favorites', FavoriteListView.as_view(), name='favorites'),
 ]
