7098a1280f2659f94f74f5e092774e50d78bdd48
3961k <80889322+3961K@users.noreply.github.com>
2021-07-07 16:08:16 +0000

    fix ルートディレクトリ(/)に対するアクセスを/articles/にリダイレクトさせる

--- config/urls.py
+++ config/urls.py
@@ -18,9 +18,11 @@ from django.contrib import admin
 from django.conf.urls.static import static
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.urls import path, include
+from django.views.generic.base import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', RedirectView.as_view(url='/articles/')),
     path('authenticate/', include('authenticate.urls')),
     path('articles/', include('articles.urls')),
     path('users/', include('users.urls')),
