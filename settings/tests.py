d24ad9980ccceca00372271c9c0a3703fac5ee08
3961k <80889322+3961K@users.noreply.github.com>
2021-07-08 16:56:44 +0000

    add フォローしているユーザを整理するためのビューを作成した
    フォローしているユーザの一覧を表示し,整理するためのビューUpdateFolloweeViewを作成した
    ビューを作成するためのテンプレートファイルとcssを作成した
    UpdateFolloweeViewに対するユニットテストの作成と検証を行った

--- settings/tests.py
+++ settings/tests.py
@@ -1,8 +1,10 @@
 from django.contrib.auth import get_user_model
 from django.test import TestCase
 from django.urls import reverse
+from urllib.parse import urlencode
 
 from .forms import UpdateUsernameForm, UpdateEmailForm
+from authenticate.models import Relation
 
 # Create your tests here.
 User = get_user_model()
@@ -168,3 +170,53 @@ class UpdateEmailViewTest(TestCase):
         # ユーザ名が同一であるかによってがメールアドレス変更されたか確認する
         user = User.objects.get(email='uevtest3@test.com')
         self.assertEquals(user.username, 'uev_tester')
+
+
+class UpdateFolloweeViewTest(TestCase):
+    @classmethod
+    def setUpClass(cls):
+        for i in range(1, 11):
+            User.objects.create_user(username='ufvtester{}'.format(i),
+                                     email='ufvtester{}@test.com'.format(i),
+                                     password='ufvtester0721')
+
+        user1 = User.objects.filter(username='ufvtester1').get()
+        for i in range(2, 11):
+            followee = User.objects.get(username='ufvtester{}'.format(i))
+            Relation.objects.create(followee=followee, follower=user1)
+
+        return super().setUpClass()
+
+    # ログインしていない場合は302が返される
+    def test_redirect_nologin(self):
+        response = self.client.get(reverse('settings:followees'))
+        self.assertEqual(response.status_code, 302)
+
+    # ログインしている場合は200を返される
+    def test_success_access(self):
+        user = User.objects.get(username='ufvtester1')
+        self.client.force_login(user)
+        response = self.client.get(reverse('settings:followees'))
+        self.assertEqual(response.status_code, 200)
+
+    # /?page=1・/?page=2にアクセスする事が出来る
+    def test_success_access_page12(self):
+        user = User.objects.get(username='ufvtester1')
+        self.client.force_login(user)
+
+        response = self.client.get(''.join([reverse('settings:followees'),
+                                            '?', urlencode(dict(page='1'))]))
+        self.assertEqual(response.status_code, 200)
+
+        response2 = self.client.get(''.join([reverse('settings:followees'),
+                                            '?', urlencode(dict(page='2'))]))
+        self.assertEqual(response2.status_code, 200)
+
+    # /?page=6(存在しないページ)にアクセスする事が出来ない
+    def test_fail_access_page_over(self):
+        user = User.objects.get(username='ufvtester1')
+        self.client.force_login(user)
+
+        response = self.client.get(''.join([reverse('settings:followees'),
+                                            '?', urlencode(dict(page='6'))]))
+        self.assertEqual(response.status_code, 404)
--- settings/urls.py
+++ settings/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from .views import UpdateUsernameView, UpdateEmailView
+from .views import UpdateUsernameView, UpdateEmailView, UpdateFolloweeView
 
 app_name = 'settings'
 
 urlpatterns = [
     path('username', UpdateUsernameView.as_view(), name='username'),
     path('email', UpdateEmailView.as_view(), name='email'),
+    path('followees', UpdateFolloweeView.as_view(), name='followees'),
 ]
