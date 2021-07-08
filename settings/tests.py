ac86944b654d781efd61cafd853edba9fc7e732d
3961k <80889322+3961K@users.noreply.github.com>
2021-07-08 19:43:53 +0000

    add パスワード変更ビューの作成
    パスワード変更を行うフォームUpdatePasswordForm,ビューUpdatePasswordViewを作成した
    パスワード変更ビューを構築するためのテンプレートファイル,cssを追加
    UpdatePasswordForm, UpdatePasswordViewに対するユニットテストの作成と検証

--- settings/tests.py
+++ settings/tests.py
@@ -1,9 +1,9 @@
 from django.contrib.auth import get_user_model
-from django.test import TestCase
+from django.test import TestCase, override_settings
 from django.urls import reverse
 from urllib.parse import urlencode
 
-from .forms import UpdateUsernameForm, UpdateEmailForm
+from .forms import UpdateUsernameForm, UpdateEmailForm, UpdatePasswordForm
 from authenticate.models import Relation
 
 # Create your tests here.
@@ -270,3 +270,106 @@ class UpdateFollowerViewTest(TestCase):
         response = self.client.get(''.join([reverse('settings:followers'),
                                             '?', urlencode(dict(page='6'))]))
         self.assertEqual(response.status_code, 404)
+
+
+class UpdatePasswordFormTest(TestCase):
+    @classmethod
+    def setUpClass(cls):
+        User.objects.create_user(username='update_password',
+                                 email='updatepassword@test.com',
+                                 password='update0123')
+        return super().setUpClass()
+
+    # 正しい形式の現パスワードと新パスワードは妥当である
+    def test_valid_form(self):
+        user = User.objects.get(username='update_password')
+        new_password_data = {
+            'old_password': 'update0123',
+            'new_password1': 'updated0123',
+            'new_password2': 'updated0123',
+        }
+        form = UpdatePasswordForm(user, new_password_data)
+        self.assertTrue(form.is_valid())
+
+    # 現在のパスワードが間違っている場合は妥当でない
+    def test_invalid_wrong_password(self):
+        user = User.objects.get(username='update_password')
+        new_password_data = {
+            'old_password': 'wrongpassword',
+            'new_password1': 'updated0123',
+            'new_password2': 'updated0123',
+        }
+        form = UpdatePasswordForm(user, new_password_data)
+        self.assertFalse(form.is_valid())
+
+    # 1つ目と2つ目の新しいパスワードが一致しない場合は妥当でない
+    def test_invalid_mismatched_newpassword(self):
+        user = User.objects.get(username='update_password')
+        new_password_data = {
+            'old_password': 'update0123',
+            'new_password1': 'updated0123',
+            'new_password2': 'updated01234',
+        }
+        form = UpdatePasswordForm(user, new_password_data)
+        self.assertFalse(form.is_valid())
+
+
+@override_settings(AXES_ENABLED=False)
+class UpdatePasswordViewTest(TestCase):
+    @classmethod
+    def setUpClass(cls):
+        User.objects.create_user(username='update_password_view',
+                                 email='updatepasswordview@test.com',
+                                 password='update0123')
+        return super().setUpClass()
+
+    # ログインしている状態でアクセスする事が出来る
+    def test_success_access(self):
+        self.client.login(username='update_password_view',
+                          password='update0123')
+        response = self.client.get(reverse('settings:password'))
+        self.assertEqual(response.status_code, 200)
+
+    # ログインしていない状態ではアクセスする事が出来ない
+    def test_fail_access(self):
+        response = self.client.get(reverse('settings:password'))
+        self.assertEqual(response.status_code, 302)
+
+    # 正しい形式の新しいパスワードによって更新する事が出来る
+    def test_success_update(self):
+        self.client.login(username='update_password_view',
+                          password='update0123')
+        update_data = {
+            'old_password': 'update0123',
+            'new_password1': 'updated0123',
+            'new_password2': 'updated0123',
+        }
+        response = self.client.post(reverse('settings:password'),
+                                    update_data, format='text/html')
+        self.assertEqual(response.status_code, 302)
+
+    # 現在のパスワードが間違っている場合は更新出来ない
+    def test_fail_update_wrong_password(self):
+        self.client.login(username='update_password_view',
+                          password='update0123')
+        wrong_password_data = {
+            'old_password': 'wrong_password',
+            'new_password1': 'updated0123',
+            'new_password2': 'updated0123',
+        }
+        response = self.client.post(reverse('settings:password'),
+                                    wrong_password_data, format='text/html')
+        self.assertNotEqual(response.status_code, 302)
+
+    # 1つ目と2つ目の新しいパスワードが一致しない場合は更新出来ない
+    def test_fail_update_missmatched_password(self):
+        self.client.login(username='update_password_view',
+                          password='update0123')
+        missmatched_password = {
+            'old_password': 'update0123',
+            'new_password1': 'updated0123',
+            'new_password2': 'updated01234',
+        }
+        response = self.client.post(reverse('settings:password'),
+                                    missmatched_password, format='text/html')
+        self.assertNotEqual(response.status_code, 302)
--- settings/urls.py
+++ settings/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from .views import UpdateUsernameView, UpdateEmailView, UpdateFolloweeView, UpdateFollowerView
+from .views import UpdateUsernameView, UpdateEmailView, UpdateFolloweeView, UpdateFollowerView, UpdatePasswordView
 
 app_name = 'settings'
 
@@ -9,4 +9,5 @@ urlpatterns = [
     path('email', UpdateEmailView.as_view(), name='email'),
     path('followees', UpdateFolloweeView.as_view(), name='followees'),
     path('followers', UpdateFollowerView.as_view(), name='followers'),
+    path('password', UpdatePasswordView.as_view(), name='password'),
 ]
